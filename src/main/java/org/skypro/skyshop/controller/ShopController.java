c681576a20ae8b5618fe67b26b80515fd16d3f31
yumaad <yudorzhieva@gmail.com>
2025-03-26 13:43:16 +0800

    Whole hw #2 Spring Boot

--- src/main/java/org/skypro/skyshop/controller/ShopController.java
+++ src/main/java/org/skypro/skyshop/controller/ShopController.java
@@ -1,28 +1,33 @@
 package org.skypro.skyshop.controller;
 
 import org.skypro.skyshop.model.article.Article;
+import org.skypro.skyshop.model.basket.UserBasket;
 import org.skypro.skyshop.model.product.Product;
 import org.skypro.skyshop.model.search.SearchResult;
+import org.skypro.skyshop.service.BasketService;
 import org.skypro.skyshop.service.SearchService;
 import org.skypro.skyshop.service.StorageService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/shop")
 public class ShopController {
     private final StorageService storageService;
     private final SearchService searchService;
+    private final BasketService basketService;
 
-    public ShopController(StorageService storageService, SearchService searchService) {
+    public ShopController(StorageService storageService,
+                          SearchService searchService,
+                          BasketService basketService) {
         this.storageService = storageService;
         this.searchService = searchService;
+        this.basketService = basketService;
     }
 
+    // Старые методы
     @GetMapping("/products")
     public Collection<Product> getAllProducts() {
         return storageService.getAllProducts();
@@ -36,5 +41,16 @@ public class ShopController {
     @GetMapping("/search")
     public Collection<SearchResult> search(@RequestParam String pattern) {
         return searchService.search(pattern);
+    }
+
+    @GetMapping("/basket/{id}")
+    public String addProductToBasket(@PathVariable UUID id) {
+        basketService.addProduct(id);
+        return "Product successfully added";
+    }
+
+    @GetMapping("/basket")
+    public UserBasket getUserBasket() {
+        return basketService.getUserBasket();
     }
 }
--- src/main/java/org/skypro/skyshop/service/StorageService.java
+++ src/main/java/org/skypro/skyshop/service/StorageService.java
@@ -50,4 +50,8 @@ public class StorageService {
         searchables.addAll(articles.values());
         return searchables;
     }
+
+    public Optional<Product> getProductById(UUID id) {
+        return Optional.ofNullable(products.get(id));
+    }
 }
